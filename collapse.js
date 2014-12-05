edc55a03abcd5fd14acca31a3d840b6815be51a0
Trace Pillars <trace.pillars@e7solutions.com>
2014-12-05 14:47:05 -0500

    should collapse children now

--- collapse.js
+++ collapse.js
@@ -4,7 +4,7 @@ for(var i = 0; i < defaults.length; i++){
     var collapse = document.createElement('a');
     collapse.innerHTML = ' [-] ';
     collapse.className = 'collapse';
-    collapse.onclick = (function(collapse, comment){
+    collapse.onclick = (function(collapse, comment, indent){
         return function(){
             if(comment.classList.contains("hidden")){
                 comment.classList.remove("hidden");
@@ -13,8 +13,19 @@ for(var i = 0; i < defaults.length; i++){
                 comment.classList.add("hidden");
                 collapse.innerHTML = ' [+] ';
             }
+
+            var currentRow = comment.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.nextSibling;
+            while(currentRow){
+                var currentRowIndent = currentRow.getElementsByTagName("img")[0].width;
+                if(currentRowIndent > indent){
+                    currentRow.classList.toggle("hidden");
+                    currentRow = currentRow.nextSibling;
+                } else{
+                    break;
+                }
+            }
         }
-    })(collapse, defaults[i].getElementsByClassName("comment")[0]);
+    })(collapse, defaults[i].getElementsByClassName("comment")[0], defaults[i].parentNode.getElementsByTagName("img")[0].width);
 
     var div = defaults[i].getElementsByTagName("div")[0];
     div.insertBefore(collapse, div.firstChild);
