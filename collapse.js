817b1d7c929f2967bb79c62b904fcb263b37860b
Trace Pillars <trace.pillars@e7solutions.com>
2015-01-23 10:16:27 -0500

    fixes overlapping collapse button for deleted comments

--- collapse.js
+++ collapse.js
@@ -1,33 +1,38 @@
 var defaults = document.getElementsByClassName('default');
 
 for(var i = 0; i < defaults.length; i++){
+    var td = defaults[i];
     var collapse = document.createElement('a');
     collapse.innerHTML = ' [-] ';
     collapse.className = 'collapse';
     collapse.onclick = (function(collapse, comment, indent){
         return function(){
-            if(comment.classList.contains("hidden")){
-                comment.classList.remove("hidden");
+            if(comment.classList.contains('hidden')){
+                comment.classList.remove('hidden');
                 collapse.innerHTML = ' [-] ';
             } else{
-                comment.classList.add("hidden");
+                comment.classList.add('hidden');
                 collapse.innerHTML = ' [+] ';
             }
 
             var currentRow = comment.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.nextSibling;
             while(currentRow){
-                var currentRowIndent = currentRow.getElementsByTagName("img")[0].width;
+                var currentRowIndent = currentRow.getElementsByTagName('img')[0].width;
                 if(currentRowIndent > indent){
-                    currentRow.classList.toggle("hidden");
+                    currentRow.classList.toggle('hidden');
                     currentRow = currentRow.nextSibling;
                 } else{
                     break;
                 }
             }
         }
-    })(collapse, defaults[i].getElementsByClassName("comment")[0], defaults[i].parentNode.getElementsByTagName("img")[0].width);
+    })(collapse, td.getElementsByClassName('comment')[0], td.parentNode.getElementsByTagName('img')[0].width);
 
-    var div = defaults[i].getElementsByTagName("div")[0];
-    div.insertBefore(collapse, div.firstChild);
+    var div = td.getElementsByTagName('div')[0];
+    if(div.firstChild.childNodes.length === 1){
+        td.insertBefore(collapse, td.lastChild);
+    } else {
+        div.insertBefore(collapse, div.firstChild);
+    }
 }
 
