a75d7434ac376242175b41728aafb8c866f2687c
Trace Pillars <trace.pillars@e7solutions.com>
2015-05-21 11:15:14 -0400

    fixes weird comment changes of expanding and not expanding by checking who collapsed each comment last

--- collapse.js
+++ collapse.js
@@ -1,14 +1,28 @@
 (function () {
     'use strict';
     var comments = document.getElementsByClassName('athing');
-    var commentsLength = comments.length;
+    if(comments.length === 0){
+        return;
+    }
 
-    // We start at i=1 because the first .athing is the submission title
-    for (var i = 1; i < commentsLength; i++) {
+    var lastCommentIndex = 0;
+    // Some pages have a submission title that is also a .athing that we do not need to process
+    if (comments[0].getElementsByClassName('title').length !== 0) {
+        lastCommentIndex = 1;
+    }
+
+    for (var i = comments.length - 1; i >= lastCommentIndex; i--) {
         var currentComment = comments[i];
         var currentCommentHeader = currentComment.getElementsByClassName('comhead')[0];
-        var currentCommentBody = currentComment.getElementsByClassName('comment')[0];
+        var currentCommentBody = currentComment.getElementsByClassName('default')[0];
         var collapseTag = document.createElement('a');
+
+        // Need to use indentation to figure out how far down in the tree it is since the comments are actually flat
+        currentComment.dataset.indentation = currentComment.querySelector('.ind img').width;
+
+        // Use hidden by to figure out what parent collapsed a certain comment so we don't end up with trees
+        // that go expanded -> collapsed -> expanded
+        currentComment.dataset.hiddenBy = -1;
 
         collapseTag.textContent = ' [-] ';
         collapseTag.className = 'collapse';
@@ -19,12 +33,34 @@
 
     function getCollapseFunction(comment, commentHeader, commentBody) {
         return function () {
-            if (commentBody.classList.contains('hidden')) {
+            var alreadyHidden = commentBody.classList.contains('hidden');
+            if (alreadyHidden) {
                 commentBody.classList.remove('hidden');
                 this.textContent = ' [-] ';
             } else {
                 commentBody.classList.add('hidden');
-                this.textContent = ' [+] ';
+                this.textContent = ' [+] '
+            }
+
+            var parentIndentation = parseInt(comment.dataset.indentation);
+            var nextElementSibling = comment.nextElementSibling;
+            while (nextElementSibling !== null) {
+                var nextIndentation = parseInt(nextElementSibling.dataset.indentation);
+                if (nextIndentation > parentIndentation) {
+                    var nextHiddenBy = parseInt(nextElementSibling.dataset.hiddenBy);
+                    if(nextHiddenBy === parentIndentation || nextHiddenBy === -1) {
+                        if (alreadyHidden) {
+                            nextElementSibling.classList.remove('hidden');
+                            nextElementSibling.dataset.hiddenBy = -1;
+                        } else {
+                            nextElementSibling.classList.add('hidden');
+                            nextElementSibling.dataset.hiddenBy = parentIndentation;
+                        }
+                    }
+                    nextElementSibling = nextElementSibling.nextElementSibling;
+                } else {
+                    break;
+                }
             }
         }
     }
