4c5d6e7751b541353305e7aa8b09a883c951e75b
Trace Pillars <trace.pillars@e7solutions.com>
2015-05-21 08:30:03 -0400

    refactor getCollapseFunction

--- collapse.js
+++ collapse.js
@@ -3,19 +3,30 @@
     var comments = document.getElementsByClassName('athing');
     var commentsLength = comments.length;
 
-    for (var i = 0; i < commentsLength; i++) {
+    // We start at i=1 because the first .athing is the submission title
+    for (var i = 1; i < commentsLength; i++) {
         var currentComment = comments[i];
         var currentCommentHeader = currentComment.getElementsByClassName('comhead')[0];
+        var currentCommentBody = currentComment.getElementsByClassName('comment')[0];
         var collapseTag = document.createElement('a');
+
         collapseTag.textContent = ' [-] ';
         collapseTag.className = 'collapse';
-        collapseTag.onclick = collapseFunction;
+        collapseTag.onclick = getCollapseFunction(currentComment, currentCommentHeader, currentCommentBody);
+
         currentCommentHeader.insertBefore(collapseTag, currentCommentHeader.firstChild);
     }
 
-    function collapseFunction(){
-        this.parentNode.parentNode.parentNode.getElementsByClassName('comment')[0].classList.toggle('hidden');
-        this.textContent = ' [+] ';
+    function getCollapseFunction(comment, commentHeader, commentBody) {
+        return function () {
+            if (commentBody.classList.contains('hidden')) {
+                commentBody.classList.remove('hidden');
+                this.textContent = ' [-] ';
+            } else {
+                commentBody.classList.add('hidden');
+                this.textContent = ' [+] ';
+            }
+        }
     }
 })();
 
