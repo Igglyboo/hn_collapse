24935ab8876921b8fabde7f138fd63c45db2eda1
Trace Pillars <trace.pillars@e7solutions.com>
2015-05-21 08:05:26 -0400

    toggle working for individual comments

--- collapse.js
+++ collapse.js
@@ -9,7 +9,13 @@
         var collapseTag = document.createElement('a');
         collapseTag.textContent = ' [-] ';
         collapseTag.className = 'collapse';
+        collapseTag.onclick = collapseFunction;
         currentCommentHeader.insertBefore(collapseTag, currentCommentHeader.firstChild);
+    }
+
+    function collapseFunction(){
+        this.parentNode.parentNode.parentNode.getElementsByClassName('comment')[0].classList.toggle('hidden');
+        this.textContent = ' [+] ';
     }
 })();
 
